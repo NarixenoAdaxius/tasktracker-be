ffb44391da6ae2eb374a265ed50d573b95a33941
NarixenoAdaxius <dauis.arionejohn@gmail.com>
2024-08-24 01:49:32 +0800

    Deployment Ready

---
+++ server/config/allowedOrigins.js
@@ -0,0 +1,7 @@
+const allowedOrigins = [
+     process.env.FRONTEND_URL,
+    'http://localhost:3000',
+    'http://localhost:3500'
+]
+
+module.exports = allowedOrigins
--- server/server.js
+++ server/server.js
@@ -11,13 +11,17 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+//Cors Configuration
+app.use(cors(corsOptions));
+app.use(express.json());
+
 // Connect to database
 connectDB();
 
-// Routes (we'll add these later)
+// Routes
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/tasks', require('./routes/taskRoutes'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
